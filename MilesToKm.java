621e7f14fefacc159b2cdffb70e0b9f11b90a3b5
JonathanBeach9 <156387910+JonathanBeach9@users.noreply.github.com>
2024-04-25 22:45:28 -0400

    Add files via upload

---
+++ MilesToKm.java
@@ -0,0 +1,21 @@
+//Jonathan Beach
+public class MilesToKm {
+    public static void main(String [] args) {
+        String[] miles = new String [] {
+            "1 mile = 1.609 km",
+            "2 miles = 3.218 km",
+            "3 miles = 4.827 km",
+            "4 miles = 6.436 km",
+            "5 miles = 8.045 km",
+            "6 miles = 9.654 km",
+            "7 miles = 11.263 km",
+            "8 miles = 12.872 km",
+            "9 miles = 14.481 km",
+            "10 miles = 16.090 km"
+        };
+        for (String mile: miles) {
+            System.out.println(mile);
+        }
+    }
+    
+}
